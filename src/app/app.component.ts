a6414aa247a4e77f1e05989cafb88ff42929ea43
hedi <hedi.brahim@gmail.com>
2021-05-07 21:19:30 +0100

    test add document to cloud firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+
+export interface Item { name: string; }
 
 @Component({
   selector: 'app-root',
@@ -11,10 +13,27 @@ export class AppComponent {
   title = 'petaid';
   public kendokaAvatar = 'https://www.telerik.com/kendo-angular-ui-develop/components/navigation/appbar/assets/kendoka-angular.png';  
 
+  private itemsCollection: AngularFirestoreCollection<Item>;
+
+  items: Observable<Item[]>;
+
+  constructor(private afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<Item>('pets');
+    this.items = this.itemsCollection.valueChanges();
+  }
+  addItem(item: Item) {
+    this.itemsCollection.add(item);
+  }
+
   item$: Observable<any[]>;
 
-  constructor(firestore: AngularFirestore) {
-    this.item$ = firestore.collection('pets').valueChanges();
-  } 
+  //constructor(firestore: AngularFirestore) {
+  //  this.item$ = firestore.collection('pets').valueChanges();
+  //} 
+
+  public onButtonClick(): void {
+    console.log('click');
+    this.addItem({name:"hedi tayara"});
+}
 
 }
